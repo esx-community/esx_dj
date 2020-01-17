67ecb68093294dc5bf65ba7e307281905a30da53
rex2630 <rex2630@gmail.com>
2020-01-17 23:15:35 +0100

    Replace __resource.lua with fxmanifest.lua

---
+++ es_mp3/fxmanifest.lua
@@ -0,0 +1,16 @@
+fx_version 'adamant'
+
+game 'gta5'
+
+description 'es_mp3.'
+
+dependency 'essentialmode'
+
+client_script 'es_mp3_client.lua'
+
+client_scripts {
+  'es_mp3_client.lua',
+	'config.lua'
+}
+
+server_script 'es_mp3_server.lua'
